24b85bf00ba915031e219a71fc6d20cdb6887ade
beaudan <beaudan.brown@gmail.com>
2026-04-28 16:35:08 +1000

    Expand FWC MAPD coverage

--- Test/FwcMapdSyncSpec.hs
+++ Test/FwcMapdSyncSpec.hs
@@ -1,15 +1,22 @@
 module Test.FwcMapdSyncSpec where
 
 import Application.FwcMapd.Sync
+import qualified Application.FwcMapd.Sync as FwcMapd
+import Application.Helper.FwcMapd
 import qualified Data.Aeson as Aeson
+import qualified Data.ByteString.Lazy.Char8 as LByteString
 import Data.Scientific (Scientific)
 import Data.Time.Calendar (fromGregorian)
 import Generated.Enums (AwardPenaltyKindEnum (..))
+import Generated.Types
+import IHP.ControllerPrelude
 import IHP.Prelude
+import IHP.Test.Mocking
 import Test.Hspec
+import Test.Support
 
 tests :: Spec
-tests =
+tests = do
     describe "FWC MAPD sync curation" do
         it "keeps only latest active adult hourly bar/pub rates and referenced classifications" do
             let asOfDate = fromGregorian 2026 4 24
@@ -20,17 +27,17 @@ tests =
                 casinoIntroClassification = classificationPayloadWithClause 106 2025 "Introductory level" Nothing (Just "Casino Gaming Employees")
                 oldClassification = classificationPayload 103 2024 "Level 2" (Just "Food and beverage attendant grade 2")
                 level1Rate =
-                    (payRatePayload
-                        101
-                        2025
-                        "AD"
-                        "Level 1"
-                        (Just "Food and beverage attendant grade 1")
-                        Nothing
-                        (Just "Weekly")
-                        (Just 24.95)
-                        (Just "Hourly"))
-                        { basePayRateId = Just "BR1" }
+                    payRatePayloadWithBasePayRateId "BR1"
+                        (payRatePayload
+                            101
+                            2025
+                            "AD"
+                            "Level 1"
+                            (Just "Food and beverage attendant grade 1")
+                            Nothing
+                            (Just "Weekly")
+                            (Just 24.95)
+                            (Just "Hourly"))
                 juniorRate =
                     payRatePayload
                         104
@@ -43,17 +50,17 @@ tests =
                         Nothing
                         Nothing
                 casinoRate =
-                    (payRatePayload
-                        102
-                        2025
-                        "AD"
-                        "Level 1"
-                        (Just "Casino electronic gaming employee grade 1")
-                        Nothing
-                        (Just "Weekly")
-                        (Just 26.38)
-                        (Just "Hourly"))
-                        { basePayRateId = Just "BR2" }
+                    payRatePayloadWithBasePayRateId "BR2"
+                        (payRatePayload
+                            102
+                            2025
+                            "AD"
+                            "Level 1"
+                            (Just "Casino electronic gaming employee grade 1")
+                            Nothing
+                            (Just "Weekly")
+                            (Just 26.38)
+                            (Just "Hourly"))
                 weeklyOnlyRate =
                     payRatePayload
                         105
@@ -126,6 +133,125 @@ tests =
             map (normaliseTimePenaltyKind . fst) wageAllowances `shouldBe` [Just EveningAfter7Pm, Just LateNightAfterMidnight]
             map (wageAllowancePayloadAmount . fst) wageAllowances `shouldBe` [Just 2.81, Just 4.22]
 
+        it "keeps casual ordinary penalties for casual base-rate projection but excludes casual overtime" do
+            let asOfDate = fromGregorian 2026 4 24
+                award2025 = awardPayload 2025
+                level1Classification = classificationPayload 101 2025 "Level 1" (Just "Food and beverage attendant grade 1")
+                level1Rate =
+                    payRatePayloadWithBasePayRateId "BR1"
+                        (payRatePayload
+                            101
+                            2025
+                            "AD"
+                            "Level 1"
+                            (Just "Food and beverage attendant grade 1")
+                            Nothing
+                            (Just "Weekly")
+                            (Just 24.95)
+                            (Just "Hourly"))
+                casualOrdinaryPenalty =
+                    (penaltyRatePayload 101 2025 "Level 1" (Just "Food and beverage attendant grade 1") "Casual employees - ordinary hours" (Just 31.19))
+                        { penaltyBasePayRateId = Just "BR1" }
+                casualOvertimePenalty =
+                    (penaltyRatePayload 101 2025 "Level 1" (Just "Food and beverage attendant grade 1") "Casual employees - overtime ordinary hours" (Just 44.10))
+                        { penaltyBasePayRateId = Just "BR1" }
+
+                (_, _, _, _, penalties) =
+                    curateAwardData
+                        barVenueCurationProfile
+                        asOfDate
+                        ( 9
+                        , [(award2025, Aeson.Null)]
+                        , [(level1Classification, Aeson.Null)]
+                        , [(level1Rate, Aeson.Null)]
+                        , [(casualOrdinaryPenalty, Aeson.Null), (casualOvertimePenalty, Aeson.Null)]
+                        )
+
+            map (payloadPenaltyDescription . fst) penalties `shouldBe` [Just "Casual employees - ordinary hours"]
+            map (normalisePenaltyKind . fst) penalties `shouldBe` [Nothing]
+
+        it "decodes text-ish MAPD fields from numbers and booleans" do
+            let decoded =
+                    Aeson.eitherDecode
+                        (LByteString.pack "{\"classification_fixed_id\":101,\"classification\":\"Level 1\",\"classification_level\":1,\"clauses\":[],\"operative_from\":\"2025-07-01\",\"published_year\":2025}") ::
+                        Either String ClassificationPayload
+
+            fmap payloadClassificationLevel decoded `shouldBe` Right (Just "1.0")
+
+    beforeAll testContext do
+        describe "FWC MAPD admin data" do
+            it "builds the support/admin award-rate view model from current core hospitality rows" $ withContext do
+                withCleanDb do
+                    award <-
+                        newRecord @FwcMapdAward
+                            |> set #awardFixedId 9
+                            |> set #awardId 9009
+                            |> set #code "MA000009"
+                            |> set #name "Hospitality Industry (General) Award 2020"
+                            |> set #awardOperativeTo (Nothing :: Maybe Day)
+                            |> createRecord
+                    _ <-
+                        newRecord @FwcMapdAward
+                            |> set #awardFixedId 99
+                            |> set #awardId 9099
+                            |> set #code "OLD"
+                            |> set #name "Closed Award"
+                            |> set #awardOperativeTo (Just (fromGregorian 2024 6 30))
+                            |> createRecord
+                    coreClassification <-
+                        newRecord @FwcMapdClassification
+                            |> set #awardFixedId award.awardFixedId
+                            |> set #classificationFixedId 101
+                            |> set #classification "Level 1"
+                            |> set #parentClassificationName (Just "Food and beverage attendant grade 1")
+                            |> set #clauseDescription (Just "Hospitality employees")
+                            |> set #operativeTo (Nothing :: Maybe Day)
+                            |> createRecord
+                    _ <-
+                        newRecord @FwcMapdClassification
+                            |> set #awardFixedId award.awardFixedId
+                            |> set #classificationFixedId 102
+                            |> set #classification "Level 1"
+                            |> set #parentClassificationName (Just "Casino electronic gaming employee grade 1")
+                            |> set #operativeTo (Nothing :: Maybe Day)
+                            |> createRecord
+                    _ <-
+                        newRecord @FwcMapdClassification
+                            |> set #awardFixedId award.awardFixedId
+                            |> set #classificationFixedId 103
+                            |> set #classification "Loaded Rate"
+                            |> set #clauseDescription (Just "LoadedRates schedule")
+                            |> set #operativeTo (Nothing :: Maybe Day)
+                            |> createRecord
+                    _ <-
+                        newRecord @FwcMapdPayRate
+                            |> set #awardFixedId award.awardFixedId
+                            |> set #classificationFixedId (Just coreClassification.classificationFixedId)
+                            |> set #classification coreClassification.classification
+                            |> set #parentClassificationName coreClassification.parentClassificationName
+                            |> set #employeeRateTypeCode (Just "AD")
+                            |> set #calculatedRate (Just 24.95)
+                            |> set #calculatedRateType (Just "Hourly")
+                            |> set #operativeTo (Nothing :: Maybe Day)
+                            |> createRecord
+                    _ <-
+                        newRecord @FwcMapdPayRate
+                            |> set #awardFixedId award.awardFixedId
+                            |> set #classificationFixedId (Just coreClassification.classificationFixedId)
+                            |> set #classification "Junior Level 1"
+                            |> set #employeeRateTypeCode (Just "JN")
+                            |> set #calculatedRate (Just 19.95)
+                            |> set #calculatedRateType (Just "Hourly")
+                            |> set #operativeTo (Nothing :: Maybe Day)
+                            |> createRecord
+
+                    adminData <- fetchFwcMapdAdminData
+
+                    map (.code) adminData.currentAwards `shouldBe` ["MA000009"]
+                    map (.classificationFixedId) adminData.currentCoreClassifications `shouldBe` [101]
+                    map (.classification) adminData.currentCoreAdultPayRates `shouldBe` ["Level 1"]
+                    map (.calculatedRate) adminData.currentCoreAdultPayRates `shouldBe` [Just 24.95]
+
 awardPayloadPublishedYear :: AwardPayload -> Maybe Int
 awardPayloadPublishedYear payload = payload.publishedYear
 
@@ -137,6 +263,12 @@ specPayRatePayloadClassificationFixedId payload = payload.classificationFixedId
 
 specPenaltyRatePayloadClassificationFixedId :: PenaltyRatePayload -> Maybe Int
 specPenaltyRatePayloadClassificationFixedId payload = payload.penaltyClassificationFixedId
+
+payloadPenaltyDescription :: PenaltyRatePayload -> Maybe Text
+payloadPenaltyDescription payload = payload.penaltyDescription
+
+payloadClassificationLevel :: ClassificationPayload -> Maybe Text
+payloadClassificationLevel payload = payload.classificationLevel
 
 wageAllowancePayloadAmount :: WageAllowancePayload -> Maybe Scientific
 wageAllowancePayloadAmount payload = payload.wageAllowanceAmount
@@ -231,6 +363,10 @@ payRatePayload classificationFixedId publishedYear employeeRateTypeCode classifi
         , versionNumber = Just 1
         , lastModifiedDatetime = Nothing
         }
+
+payRatePayloadWithBasePayRateId :: Text -> PayRatePayload -> PayRatePayload
+payRatePayloadWithBasePayRateId value payload =
+    payload { FwcMapd.basePayRateId = Just value }
 
 wageAllowancePayload :: Int -> Text -> Scientific -> Scientific -> WageAllowancePayload
 wageAllowancePayload publishedYear allowance rate allowanceAmount =
