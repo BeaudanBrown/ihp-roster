9d66f169336b0099d70678ef53047fb89ff0b292
Beaudan Brown <beaudan.brown@gmail.com>
2026-05-19 13:00:36 +1000

    Cover concurrent support job enqueue

--- Test/Controller/SupportSpec.hs
+++ Test/Controller/SupportSpec.hs
@@ -1,5 +1,7 @@
 module Test.Controller.SupportSpec where
 
+import Application.Async.Queue (activeAppJobStatuses)
+import Application.FwcMapd.Job (fwcMapdRefreshJobKind)
 import Application.Helper.Controller (PlatformRole (SuperAdminRole))
 import Application.Helper.LiveSurface (typedLiveSurfaceFragmentRefs,
                                        unSurfaceFragmentRefs)
@@ -7,6 +9,10 @@ import Application.Helper.LiveUpdate (currentLiveUpdateVersion)
 import Application.Helper.LiveUpdate.Internal (LiveUpdateWireFragment)
 import Application.Support.LiveUpdates
 import Config
+import Control.Concurrent (forkIO, newEmptyMVar, putMVar, takeMVar)
+import Control.Exception (SomeException, try)
+import Generated.Types
+import IHP.ControllerPrelude
 import IHP.FrameworkConfig
 import IHP.Prelude
 import IHP.Test.Mocking
@@ -75,3 +81,30 @@ tests = beforeAll testContext do
                 response `responseStatusShouldBe` status200
                 response `responseBodyShouldContain` "id=\"support-public-holidays-section\""
                 versionAfter `shouldBe` versionBefore + 1
+
+        it "deduplicates concurrent award-rate refresh enqueues without 500s" $ withContext do
+            withCleanDb do
+                superAdmin <- createUserRecordWithPlatformRole "support-concurrent-super@example.com" "staff" (Just SuperAdminRole) True
+
+                results <- runConcurrentActions 12 do
+                    withPasskeyVerifiedUser superAdmin do
+                        withRequestHeaders [("HX-Request", "true")] do
+                            callAction CreateFwcMapdRefreshJobAction
+
+                let failures = lefts results
+                failures `shouldSatisfy` null
+                let responses = rights results
+                length responses `shouldBe` 12
+                mapM_ (`responseStatusShouldBe` status200) responses
+
+                activeJobs <- query @AppJob
+                    |> filterWhere (#jobKind, fwcMapdRefreshJobKind)
+                    |> filterWhereIn (#status, activeAppJobStatuses)
+                    |> fetch
+                length activeJobs `shouldBe` 1
+
+runConcurrentActions :: Int -> IO a -> IO [Either SomeException a]
+runConcurrentActions count action = do
+    vars <- mapM (const newEmptyMVar) [1 .. count]
+    _ <- mapM (\var -> forkIO (try action >>= putMVar var)) vars
+    mapM takeMVar vars
