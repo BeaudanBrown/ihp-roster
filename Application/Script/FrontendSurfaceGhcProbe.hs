7d29a69e6e90fdfaa0e04dcdf4e4b7323c68a3b1
Beaudan Brown <beaudan.brown@gmail.com>
2026-07-09 23:24:54 +1000

    change imports

--- Application/Script/FrontendSurfaceGhcProbe.hs
+++ Application/Script/FrontendSurfaceGhcProbe.hs
@@ -7,11 +7,12 @@ import Application.Helper.FrontendContract.Surface.Ghc.Raw hiding
                                                            (rawRegistryToJson)
 import qualified Application.Helper.FrontendContract.Surface.Ghc.Raw as Raw
 import Application.Script.Prelude (Script)
+import Control.Monad.IO.Class (liftIO)
 import qualified Data.Aeson as Aeson
 import qualified Data.ByteString.Lazy.Char8 as LBS
 import qualified Data.List as List
 import qualified Data.Text as Text
-import IHP.Prelude (liftIO)
+import Prelude
 import qualified System.Environment as Environment
 import System.Exit (exitFailure)
 
