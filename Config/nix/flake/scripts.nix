106070726d1fc178f0900e826127400afd9e8d61
beaudan <beaudan.brown@gmail.com>
2026-07-29 13:40:25 +1000

    Add idempotent epic worktree provisioning (#267)

--- Config/nix/flake/scripts.nix
+++ Config/nix/flake/scripts.nix
@@ -95,6 +95,8 @@ in
         git-pre-commit-check = script ../scripts/git/pre-commit-check;
         epic-worktree = script ../scripts/epic/worktree;
         epic-worktree-test = script ../scripts/epic/worktree-test;
+        epic-worktree-provision = script ../scripts/epic/provision;
+        epic-worktree-provision-test = script ../scripts/epic/provision-test;
         architecture-contracts = script ../scripts/architecture/contracts;
         architecture-facts = script ../scripts/architecture/facts;
         architecture-schema = script ../scripts/architecture/schema;
